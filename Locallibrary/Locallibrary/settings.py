fa4c7852efdeb061f83fac1c15881a9bf71e85da
parvej-shah <parvejshahlabib007@gmail.com>
2022-02-11 11:52:22 +0600

    Part 8 done

--- Locallibrary/Locallibrary/settings.py
+++ Locallibrary/Locallibrary/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+LOGIN_REDIRECT_URL = '/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- Locallibrary/catalog/urls.py
+++ Locallibrary/catalog/urls.py
@@ -1,5 +1,35 @@
-from django.urls import path
-from . import views
 
+from django.urls import include, path
+from . import views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 urlpatterns = [
-]
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('author/', views.AuthorListView.as_view(), name='author'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path("allborrowed/", views.AllBorrowedBookListView.as_view(), name="all-borrowed"),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian,
+         name='renew-book-librarian'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/',
+         views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/',
+         views.AuthorDelete.as_view(), name='author-delete'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+""" accounts/ login/ [name='login']
+accounts/ logout/ [name='logout']
+accounts/ password_change/ [name='password_change']
+accounts/ password_change/done/ [name='password_change_done']
+accounts/ password_reset/ [name='password_reset']
+accounts/ password_reset/done/ [name='password_reset_done']
+accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
+accounts/ reset/done/ [name='password_reset_complete'] """
